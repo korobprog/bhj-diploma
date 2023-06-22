da7b8110acb75792ff7d0c930bd5a56b03be7a5f
korobprog <korobprog@gmail.com>
2023-06-22 19:07:50 +1000

    form login createRequest LoginForm

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,29 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
 const createRequest = (options = {}) => {
-
+   const xhr = new XMLHttpRequest();
+   xhr.responseType = 'json';
+   let result = '';
+   let formData = null;
+   xhr.withCredentials = true;
+   if (options.method === 'GET') {
+      if (options.data) {
+         result = `?`;
+         for (let key in options.data) {
+            result += `${key}=${options.data[key]}&`;
+         };
+      };
+   } else {
+      formData = new FormData();
+      for (let key in options.data) {
+         formData.append(key, options.data[key]);
+      };
+   };
+   try {
+      xhr.open(`${options.method}`, `${options.url}${result}`);
+      xhr.send(formData);
+   } catch (err) {
+      options.callback(err);
+   }
+   xhr.onload = function () {
+      options.callback(null, xhr.response);
+   }
 };
