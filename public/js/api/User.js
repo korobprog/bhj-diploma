25cf2938adbb554a6db2ca683c721bcf39598b8f
korobprog <korobprog@gmail.com>
2023-06-22 19:45:02 +1000

    User

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -1,55 +1,37 @@
-/**
- * Класс User управляет авторизацией, выходом и
- * регистрацией пользователя из приложения
- * Имеет свойство URL, равное '/user'.
- * */
 class User {
-  /**
-   * Устанавливает текущего пользователя в
-   * локальном хранилище.
-   * */
+  static url = '/user';
   static setCurrent(user) {
-
+    localStorage.setItem('user', JSON.stringify(user));
   }
-
-  /**
-   * Удаляет информацию об авторизованном
-   * пользователе из локального хранилища.
-   * */
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
-
-  /**
-   * Возвращает текущего авторизованного пользователя
-   * из локального хранилища
-   * */
   static current() {
-
+    return JSON.parse(localStorage.getItem('user'));
   }
 
-  /**
-   * Получает информацию о текущем
-   * авторизованном пользователе.
-   * */
   static fetch(callback) {
-
+    createRequest({
+      url: `${this.url}/current`,
+      method: 'GET',
+      callback: (err, response) => {
+        if (response.success) {
+          this.setCurrent(response.user);
+        } else {
+          this.unsetCurrent();
+        };
+        callback(err, response);
+      }
+    });
   }
 
-  /**
-   * Производит попытку авторизации.
-   * После успешной авторизации необходимо
-   * сохранить пользователя через метод
-   * User.setCurrent.
-   * */
   static login(data, callback) {
     createRequest({
-      url: this.URL + '/login',
+      url: `${this.url}/login`,
       method: 'POST',
-      responseType: 'json',
       data,
       callback: (err, response) => {
-        if (response && response.user) {
+        if (response.success) {
           this.setCurrent(response.user);
         }
         callback(err, response);
@@ -57,21 +39,30 @@ class User {
     });
   }
 
-  /**
-   * Производит попытку регистрации пользователя.
-   * После успешной авторизации необходимо
-   * сохранить пользователя через метод
-   * User.setCurrent.
-   * */
   static register(data, callback) {
-
+    createRequest({
+      url: `${this.url}/register`,
+      method: 'POST',
+      data,
+      callback: (err, response) => {
+        if (response.success) {
+          this.setCurrent(response.user);
+        };
+        callback(err, response);
+      }
+    });
   }
 
-  /**
-   * Производит выход из приложения. После успешного
-   * выхода необходимо вызвать метод User.unsetCurrent
-   * */
   static logout(callback) {
-
+    createRequest({
+      url: `${this.url}/logout`,
+      method: 'POST',
+      callback: (err, response) => {
+        if (response.success) {
+          this.unsetCurrent();
+        };
+        callback(err, response);
+      }
+    });
   }
 }
