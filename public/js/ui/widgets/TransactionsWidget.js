1076ff3f81a89a697bd545d034a2bb28ae91cefc
korobprog <korobprog@gmail.com>
2023-06-22 19:18:10 +1000

    TransactionsWidget

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -1,26 +1,20 @@
-/**
- * Класс TransactionsWidget отвечает за
- * открытие всплывающих окон для
- * создания нового дохода или расхода
- * */
-
 class TransactionsWidget {
-  /**
-   * Устанавливает полученный элемент
-   * в свойство element.
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor( element ) {
-
+  constructor(element) {
+    if (!element) {
+      throw new Error('ошибка');
+    };
+    this.element = element;
+    this.registerEvents();
   }
-  /**
-   * Регистрирует обработчики нажатия на
-   * кнопки «Новый доход» и «Новый расход».
-   * При нажатии вызывает Modal.open() для
-   * экземпляра окна
-   * */
-  registerEvents() {
 
+  registerEvents() {
+    this.element.addEventListener('click', (event) => {
+      if (event.target.classList.contains('create-income-button')) {
+        App.getModal('newIncome').open();
+      }
+      if (event.target.classList.contains('create-expense-button')) {
+        App.getModal('newExpense').open();
+      }
+    })
   }
 }
