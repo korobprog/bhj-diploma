c85828f2f19dbff0666800f8b7b126a6a7655e6e
korobprog <korobprog@gmail.com>
2023-06-22 19:12:36 +1000

    Sidebar

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,12 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
-
+    const bodySidebar = document.querySelector('.sidebar-mini');
+    const button = document.querySelector('.sidebar-toggle');
+    button.addEventListener('click', () => {
+      bodySidebar.classList.toggle('sidebar-open');
+      bodySidebar.classList.toggle('sidebar-collapse');
+    });
   }
 
   /**
@@ -29,6 +34,26 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
+    const menuRegister = document.querySelector('.menu-item_register');
 
+    const menuLogin = document.querySelector('.menu-item_login');
+
+    const menuLogout = document.querySelector('.menu-item_logout');
+
+    menuRegister.addEventListener('click', () => {
+      App.getModal('register').open();
+    });
+
+    menuLogin.addEventListener('click', () => {
+      App.getModal('login').open();
+    });
+
+    menuLogout.addEventListener('click', () => {
+      User.logout((err, response) => {
+        if (response.success) {
+          App.setState('init');
+        }
+      })
+    });
   }
 }
