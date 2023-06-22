b5f1b390c792fe9121fad52f61b7bc40fdbb78c2
korobprog <korobprog@gmail.com>
2023-06-22 19:38:49 +1000

    RegisterForm

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -1,15 +1,11 @@
-/**
- * Класс RegisterForm управляет формой
- * регистрации
- * */
 class RegisterForm extends AsyncForm {
-  /**
-   * Производит регистрацию с помощью User.register
-   * После успешной регистрации устанавливает
-   * состояние App.setState( 'user-logged' )
-   * и закрывает окно, в котором находится форма
-   * */
   onSubmit(data) {
-
+    User.register(data, (err, response) => {
+      if (response.success) {
+        App.setState('user-logged');
+        this.element.reset();
+        App.getModal('register').close();
+      }
+    })
   }
 }
