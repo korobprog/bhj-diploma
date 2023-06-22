cb4d118597fa9ad0033fd111b5d4d61c8d4bafd9
korobprog <korobprog@gmail.com>
2023-06-22 19:14:51 +1000

    App

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -1,22 +1,8 @@
-/**
- * Класс Sidebar отвечает за работу боковой колонки:
- * кнопки скрытия/показа колонки в мобильной версии сайта
- * и за кнопки меню
- * */
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
   }
-
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
   static initToggleButton() {
     const bodySidebar = document.querySelector('.sidebar-mini');
     const button = document.querySelector('.sidebar-toggle');
@@ -25,14 +11,6 @@ class Sidebar {
       bodySidebar.classList.toggle('sidebar-collapse');
     });
   }
-
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
   static initAuthLinks() {
     const menuRegister = document.querySelector('.menu-item_register');
 
